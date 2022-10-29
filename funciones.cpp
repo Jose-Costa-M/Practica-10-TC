0607a17bf874051eebdda53e12fd8eedc25ee8fe
José Costa <88689761+Jose-Costa-M@users.noreply.github.com>
2022-10-29 15:01:44 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -36,59 +36,88 @@ void eleccion_rangoLetras_mayusculas (char *inicio, char *fin) {
     do {
         cout << "(SOLO ENTRE A-Z)" << endl;
         cout << "Digita el inicio del rango" << endl;
-        cin>>*inicio;
+        cin >> *inicio;
         *inicio = toupper(*inicio);
-        cout<<"Digita el fin del rango"<<endl;
-        cin>>*fin;
+        cout << "Digita el fin del rango" << endl;
+        cin >> *fin;
         *fin = toupper(*fin);
     } while (*inicio < 'A' || *fin > 'Z' || *inicio > *fin);
 }
 
-void eleccion_rangoLetras_minusculas(char *inicio, char *fin) {
-
-    do{
-        cout<<"(SOLO ENTRE a-z)"<<endl;
-        cout<<"Digita el inicio del rango"<<endl;
-        cin>>*inicio;
+void eleccion_rangoLetras_minusculas (char *inicio, char *fin) {
+    do {
+        cout << "(SOLO ENTRE a-z)" << endl;
+        cout << "Digita el inicio del rango" << endl;
+        cin >> *inicio;
         *inicio = tolower(*inicio);
-        cout<<"Digita el fin del rango"<<endl;
-        cin>>*fin;
+        cout << "Digita el fin del rango" << endl;
+        cin >> *fin;
         *fin = tolower(*fin);
     } while (*inicio < 'a' || *fin > 'z' || *inicio > *fin);
-    
 }
 
 
-void menu() {
-        cout<<"\n|---------------------------------------|";
-        cout<<"\n|       ° INTRODUCE TU ALFABETO °       |";
-        cout<<"\n|--------------------|------------------|";
-        cout<<"\n| 1. INDIVIDUAL      | 2. RANGO         |";
-        cout<<"\n|--------------------|------------------|";
-        puts("\nSelecciona una opcion:");
+void menu () {
+    cout << "\n|---------------------------------------|";
+    cout << "\n|       ° INTRODUCE TU ALFABETO °       |";
+    cout << "\n|--------------------|------------------|";
+    cout << "\n| 1. INDIVIDUAL      | 2. RANGO         |";
+    cout << "\n|--------------------|------------------|";
+    puts ("\nSelecciona una opcion:");
 }
 
-void menu_rango(){
-        cout<<"\n|---------------------------------------|";
-        cout<<"\n|           ° ELIGE LA OPCION °         |";
-        cout<<"\n|--------------------|------------------|";
-        cout<<"\n| 1. NUMEROS         | 2. LETRAS        |";
-        cout<<"\n|--------------------|------------------|";
-        puts("\nSelecciona una opcion:");
+void menu_rango () {
+    cout << "\n|---------------------------------------|";
+    cout << "\n|           ° ELIGE LA OPCION °         |";
+    cout << "\n|--------------------|------------------|";
+    cout << "\n| 1. NUMEROS         | 2. LETRAS        |";
+    cout << "\n|--------------------|------------------|";
+    puts ("\nSelecciona una opcion:");
 }
 
-void menu_rangoLetras(){
-        cout<<"\n|---------------------------------------|";
-        cout<<"\n|           ° ELIGE LA OPCION °         |";
-        cout<<"\n|--------------------|------------------|";
-        cout<<"\n| 1. MAYUSCULAS      | 2. MINUSCULAS    |";
-        cout<<"\n|--------------------|------------------|";
-        puts("\nSelecciona una opcion:");
+void menu_rangoLetras () {
+    cout << "\n|---------------------------------------|";
+    cout << "\n|           ° ELIGE LA OPCION °         |";
+    cout << "\n|--------------------|------------------|";
+    cout << "\n| 1. MAYUSCULAS      | 2. MINUSCULAS    |";
+    cout << "\n|--------------------|------------------|";
+    puts ("\nSelecciona una opcion:");
 }
 
-void imprimir_alfabeto(alfabeto a){
-    for(string c: a) {
-        cout<<"[ "<<c<<" ]"<<endl;
+void imprimir_alfabeto (alfabeto a) {
+    for (string c: a) {
+        cout << "[ "<<c<<" ]" << endl;
     }
 }
+
+bool verificar_cadena (string s1, set<string> a, int tam2) {
+    for (int i = 0; i < s1.size(); i+=tam2) {
+        if (a.find(s1.substr(i, tam2)) == a.end()) {
+            system("cls");
+             cout << "La cadena "<<s1<<" no pertenece al alfabeto" << endl;
+            return false;
+        }
+    }
+    system("cls");
+    cout << "La cadena "<<s1<<" pertenece al alfabeto" << endl;
+    return true;
+}
+//perro (1,6) --> erro   perro : erro es subfijo de perro
+
+void leer_validar_cadena (string *s1, set<string> a, int tam) {
+    int i = 0;
+    int tam2 = tam;
+    do{
+        if(i==0){
+        cout << "Digita la cadena a evaluar" << endl;
+        cin >> *s1;
+        }else{
+            cout << "Digita una cadena valida" << endl;
+            cin >> *s1;
+        }
+        i++;
+    } while (!verificar_cadena(*s1, a, tam2));
+
+}
+
 
